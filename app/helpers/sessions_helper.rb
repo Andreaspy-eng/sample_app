642ed88bfc013b6ce78bccf9a45d7c890cc9eb8d
Andrew <foki.andrew@gmail.com>
2020-12-05 19:32:15 +0300

    Finish log in/log out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -25,20 +25,9 @@ module SessionsHelper
     end
   end
 
-  # Возвращает текущего вошедшего пользователя (если есть).
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
-
   # Возвращает true, если пользователь вошел, иначе false.
   def logged_in?
     !current_user.nil?
-  end
-
-  # Осуществляет выход текущего пользователя.
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
   end
 
   # Забывает постоянную сессии.
