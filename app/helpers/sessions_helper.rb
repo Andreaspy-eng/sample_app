0f35266d31f6430bd992224f0056fc3427b0c3e4
Andrew <foki.andrew@gmail.com>
2020-12-06 17:01:11 +0300

    Finish user edit, update, index, and destroy actions with errors

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,18 +12,23 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-   # Возвращает пользователя, соответствующего remember-токену в куки.
-   def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
+  # Возвращает true, если заданный пользователь является текущим.
+  def current_user?(user)
+    user == current_user
+  end
+
+  # Возвращает пользователя, соответствующего remember-токену в куки.
+  def current_user
+  if (user_id = session[:user_id])
+    @current_user ||= User.find_by(id: user_id)
+  elsif (user_id = cookies.signed[:user_id])
+    user = User.find_by(id: user_id)
+    if user && user.authenticated?(cookies[:remember_token])
+      log_in user
+      @current_user = user
     end
   end
+ end
 
   # Возвращает true, если пользователь вошел, иначе false.
   def logged_in?
@@ -42,5 +47,15 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Сохраняет запрошенный URL.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
   end
 end
