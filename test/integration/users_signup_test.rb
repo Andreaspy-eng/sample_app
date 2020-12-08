10d5cba6d52937c46c719475156aad910472b0b4
Andrew <foki.andrew@gmail.com>
2020-12-08 17:06:23 +0300

    Add password resets & email configuration

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -27,23 +27,5 @@ require 'test_helper'
                                               password_confirmation: "password" } } 
           follow_redirect!
         end
-        assert_equal 1, ActionMailer::Base.deliveries.size
-        user = assigns(:user)
-        assert_not user.activated?
-        # Попытка войти до активации
-        log_in_as(user)
-        assert_not is_logged_in?
-        # Невалидный активационный токен
-        get edit_account_activation_path("invalid token")
-        assert_not is_logged_in?
-        # Валидный токен, неверный адрес электронной почты
-        get edit_account_activation_path(user.activation_token, email: 'wrong')
-        assert_not is_logged_in?
-        # Валидный активационный токен и адрес почты
-        get edit_account_activation_path(user.activation_token, email: user.email)
-        assert user.reload.activated?
-        follow_redirect!
-        assert_template 'users/show'
-        assert is_logged_in?
       end
     end
